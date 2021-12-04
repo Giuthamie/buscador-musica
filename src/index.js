ae667ae777228ec1272bfedaef0159c987a1b2ea
giuthamie <giumie@hotmail.com>
2021-12-03 21:04:23 -0300

    printando os dados na tela

--- src/index.js
+++ src/index.js
@@ -6,15 +6,23 @@ const prevAndNextContainer = document.querySelector('#prev-and-next-container');
 /* armazenando a url da api*/
 const apiUrl = `https://api.lyrics.ovh/`
 
+/*printando os dados na tela*/
+const insertSongsIntoPage = songsInfo => {    
+    songsContainer.innerHTML = songsInfo.data.map(song => `
+    <li clas="song">
+    <span class="song-artist"><strong>${song.artist.name}</strong> - ${song.title}</span>
+    <button class="btn" data-artist="${song.artist.name}" data-song-title="${song.title}"></button>
+    </li>
+    `).join('')
+}
+
 /*requisição das letras das músicas*/
 
 const fetchSongs = async term => {
     const response = await fetch(`${apiUrl}/suggest/${term}`)
     const data = await response.json()
     
-        console.log(data)
-
-
+    insertSongsIntoPage(data)
 }
 
 form.addEventListener('submit', event => {
