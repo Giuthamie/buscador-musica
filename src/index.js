019a222b117ba65642fd5ea7f08610e82ef02a13
giuthamie <giumie@hotmail.com>
2021-12-09 11:56:46 -0300

    funcionalidade de url com próximas musicas e musicas anteriores

--- src/index.js
+++ src/index.js
@@ -6,14 +6,30 @@ const prevAndNextContainer = document.querySelector('#prev-and-next-container');
 /* armazenando a url da api*/
 const apiUrl = `https://api.lyrics.ovh/`
 
+const getMoreSongs = async url => {
+    const response = await fetch(`https://cors-anywhere.herokuapp.com/${url}`)
+    const data = await response.json()
+    
+    insertSongsIntoPage(data)
+
+}
+
 /*printando os dados na tela*/
-const insertSongsIntoPage = songsInfo => {    
+const insertSongsIntoPage = songsInfo => {
     songsContainer.innerHTML = songsInfo.data.map(song => `
     <li clas="song">
     <span class="song-artist"><strong>${song.artist.name}</strong> - ${song.title}</span>
-    <button class="btn" data-artist="${song.artist.name}" data-song-title="${song.title}"></button>
+    <button class="btn" data-artist="${song.artist.name}" data-song-title="${song.title}">Ver letra</button>
     </li>
     `).join('')
+    if(songsInfo.prev || songsInfo.next){
+        prevAndNextContainer.innerHTML = `
+        ${songsInfo.prev ? `<button class="btn" onClick="getMoreSongs('${songsInfo.prev}')">Anteriores</button>` : ''}
+        ${songsInfo.next ? `<button class="btn" onClick="getMoreSongs('${songsInfo.next}')">Próximas</button>` : ''}
+        `
+        return
+    }
+    prevAndNextContainer.innerHTML = ''
 }
 
 /*requisição das letras das músicas*/
